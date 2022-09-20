a1f9f120cd77cde966e958e77efba1c99f198535
SAURABH SAINI <55580746+saurabhsaini400@users.noreply.github.com>
2022-09-20 10:19:17 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/mavenchecker/App.java
@@ -0,0 +1,13 @@
+package com.nagarro.mavenchecker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
